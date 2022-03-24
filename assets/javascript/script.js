0dfc3fabb8960e2d903a50bbdf8ad1d3caeb0380
stephen rogan <srogan26@gmail.com>
2022-03-23 20:22:50 -0500

    added functionality to search button/box

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,21 @@
 console.log("Hello World");
-//FETCH REQUEST FOR GETTING DATA FROM API
-fetch('http://www.omdbapi.com/?apikey=4619adc4&s=water%20boy')
-  .then(response => response.json())
-  .then(data => console.log(data));
+//CREATE VARIABLES FOR REFERENCING THE BUTTON AND SEARCH BOX
+var searchBtn = document.querySelector("#searchbtn");
+var searchBox = document.querySelector("#searchbox");
+//ADD LISTENER FOR BUTTON CLICK
+searchBtn.addEventListener("click", function (event) {
+  //STOP SUBMIT BUTTON FROM REFRESHING PAGE BY DEFAULT
+  event.preventDefault();
+  //CREATE VARIABLE FOR SEARCH INPUT
+  var searchValue = searchBox.value;
+  //FETCH REQUEST FOR GETTING DATA FROM API
+  fetch('http://www.omdbapi.com/?apikey=4619adc4&t='+ searchValue)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    //.CATCH IS FOR ERROR HANDLING IN FETCH REQUEST
+    .catch(error => console.log(error));
+});
+
+
+
+
