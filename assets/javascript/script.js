698598f710c2a8c5c9c68d0f470924717b562577
stephen rogan <srogan26@gmail.com>
2022-04-04 20:49:08 -0500

    Added button to result cards and fixed sizing to be dynamic to container

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,10 @@
 console.log("Hello World");
+//PLACEHOLDER IMAGE URL
 const imgPlaceholder = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/No_sign.svg/300px-No_sign.svg.png?20101202215511"
+//LIST SEARCH URL
+const listSearch = 'http://www.omdbapi.com/?apikey=4619adc4&s=';
+//BY ID SEARCH URL
+const idSearch = 'http://www.omdbapi.com/?apikey=4619adc4&i=';
 //CREATE VARIABLES FOR REFERENCING THE BUTTON AND SEARCH BOX
 var searchBtn = document.querySelector("#searchbtn");
 var searchBox = document.querySelector("#searchbox");
@@ -10,7 +15,7 @@ searchBtn.addEventListener("click", function (event) {
   //CREATE VARIABLE FOR SEARCH INPUT
   const searchValue = searchBox.value;
   //FETCH REQUEST FOR GETTING DATA FROM API
-  fetch('http://www.omdbapi.com/?apikey=4619adc4&s=' + searchValue)
+  fetch(listSearch + searchValue)
     .then(response => {
       return response.json();
     })
@@ -25,15 +30,15 @@ searchBtn.addEventListener("click", function (event) {
         const title = movie.Title;
         const year = movie.Year;
         const poster = movie.Poster;
+        const idNumber = movie.imdbID;
         //SET UP FUNCTIONALITY TO CREATE A NEW CARD HTML ELEMENT FOR EACH RESULT  
         const newCard = document.createElement('div');
         //ADD THE CARD CLASS TO OUR NEW DIV ELEMENT
         newCard.classList.add('card');
         //GIVE CARD UNIQUE ID ATTRIBUTE
-        newCard.setAttribute("id","result"+i);
-        //ADD DIMENSIONS TO CARD ELEMENT
-        newCard.style.height = '400px';
-        newCard.style.width = '200px';
+        newCard.setAttribute("id", "result" + i);
+        //ADD DIMENSIONS TO CARD ELEMENT, LETS 3 RESULTS DISPLAY PER ROW W/DYNAMIC SIZE
+        newCard.style.width = '30%';
         //CREATE IMAGE ELEMENT FOR POSTER IMAGE ON CARD
         const newPoster = document.createElement('img');
         //ADD CARD TOP CLASS FOR THE IMAGE ELEMENT
@@ -45,23 +50,26 @@ searchBtn.addEventListener("click", function (event) {
         else {
           newPoster.setAttribute("src", imgPlaceholder)
         };
-        //HEIGHT OF IMAGE TO CARD
-        newPoster.style.height = '300px';
         //CREATE CARD TEXT AREA ELEMENT
         const newTextArea = document.createElement("div");
         newTextArea.classList.add("card-body");
         //CREATE TITLE AND YEAR ELEMENTS FOR CARD TEXT
-        const titleText = document.createElement("h6");
+        const titleText = document.createElement("h5");
         titleText.classList.add("card-title");
         const yearText = document.createElement("p");
         yearText.classList.add("card-text");
-        //ADD TITLE AND YEAR TEXT OF SEARCH RESULT TO THEIR ELEMENTS
+        //ADD TITLE & YEAR TEXT OF SEARCH RESULT TO THEIR ELEMENTS
         titleText.textContent = title;
-        yearText.textContent = "Released "+year;
-        //ASSEMBLE THE CARD BODY TEXT AREA
-        newTextArea.append(titleText,yearText);
+        yearText.textContent = "Released " + year;
+        //CREATE BUTTON FOR PLOT, DISPLAYING ID AS TEXT FOR PLACEHOLDER
+        const plotBtn = document.createElement("a");
+        plotBtn.classList.add('btn', 'btn-success');
+        plotBtn.setAttribute("href", idSearch + idNumber + '&plot=full');
+        plotBtn.textContent = 'Plot: ' + idNumber;
+        //ASSEMBLE THE CARD BODY TEXT AREA W/ PLOT BUTTON
+        newTextArea.append(titleText, yearText, plotBtn);
         //ADD IMAGE AND CARD TEXT TO CARD ELEMENT
-        newCard.append(newPoster,newTextArea);
+        newCard.append(newPoster, newTextArea);
         //ADD NEW CARD TO RESULTS DISPLAY AREA
         searchResults.appendChild(newCard);
       }
